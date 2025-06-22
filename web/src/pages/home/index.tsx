80cbf893043ae940b07adf7bb5d84a2cc5cad519
Tanishq Singh <hi@tanishqsingh.com>
2025-06-22 22:20:31 +0530

    web: improvement: api-speed

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import FeatureSelection from "./feature-selection";
 
 export default function Home() {
+    useEffect(() => {
+        /*
+         * dude server do cold start, if it didn't tackle request for long
+         * so it's better to hit before user name any request
+         */
+        (async () => await fetch(`${import.meta.env.VITE_API_URL}`))();
+    }, []);
+
     return (
         <main className="flex flex-1 w-full items-center justify-center px-3 sm:px-5 md:px-8">
             <FeatureSelection />
